9380f83712b7e8c3f39ba0a54ba044cc278dc13d
Alon Nusem <61749872+4lon@users.noreply.github.com>
2020-04-14 20:23:47 +1000

    Testing kivy gardens to plot

---
+++ testplot.py
@@ -0,0 +1,29 @@
+import kivy
+#kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+from math import sin
+from kivy_garden.graph import Graph, MeshLinePlot
+
+
+import plotly.express as px
+
+class MyApp(App):
+
+    def build(self):
+        fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+        fig.show()
+        graph = Graph(xlabel='X', ylabel='Y', x_ticks_minor=5,
+        x_ticks_major=25, y_ticks_major=1,
+        y_grid_label=True, x_grid_label=True, padding=5,
+        x_grid=True, y_grid=True, xmin=-0, xmax=100, ymin=-1, ymax=1)
+        plot = MeshLinePlot(color=[1, 0, 0, 1])
+        plot.points = [(x, sin(x / 10.)) for x in range(0, 101)]
+        graph.add_plot(plot)
+        return graph
+
+
+if __name__ == '__main__':
+    MyApp().run()
