c310f68aabefc37c5b29e7cd3cfa6462d99ed435
Olivia-laptop <olive_macross@rocketmail.com>
2020-04-15 18:08:05 +1000

    adding lono

--- ArduinotoKivy.py
+++ ArduinotoKivy.py
@@ -13,41 +13,66 @@ import random
 from sounds import *
 
 clear = True
-x_amount = 200
+baudrate = 9600
+graphTime = 10000 #number milliseconds
 
 def get_data():
     global levels
-    global pastLevels
+    global times
+    global oldLevels
+    global oldTime
+    global maxTime
+    oldTime = []
     levels = []
-    pastLevels = []
+    oldLevels = []
+    times = []
+    maxTime = 0
     try:
-        ser = serial.Serial('COM3', 9600)
+        ser = serial.Serial('COM3', baudrate)
         while True:
             while (ser.inWaiting()==0):
                 pass
             value = ser.readline()
             try:
-                intValue = int(value.decode("utf-8"))
-                update_level(intValue)
+                data = str(value.decode("utf-8"))
+                data=data.split(",")
+                dataTime = int(data[0])
+                signal1 = int(data[1])
+                update_level(dataTime, signal1)
             except:
                 pass
     except:
+        currTime = 0
         while True:
             intValue = random.randint(1, 1020)
-            update_level(intValue)
-            time.sleep(0.01)
+            currTime += 200
+            update_level(currTime, intValue)
+            time.sleep(0.1)
 
-def update_level(value):
+def update_level(timeIn, value):
     global levels
-    global pastLevels
-    if len(levels) >= x_amount:
-        pastLevels.append(levels.pop(0))
-        if clear:
-            pastLevels = levels.copy()
-            levels = []
-    if len(pastLevels) >= x_amount:
-        pastLevels.pop(0)    
+    global oldLevels
+    global oldTime
+    global times
+    global maxTime
+    timeIn -= maxTime
+    if timeIn >= graphTime:
+        maxTime +=timeIn
+        oldLevels = levels.copy()
+        oldTime = times.copy()
+        levels = []
+        times = []
+        timeIn = 0
     levels.append(value)
+    times.append(timeIn)
+
+def combineLists(list1,list2):
+    list=[]
+    index=0
+    for v in list1:
+        list.append((list1[index],list2[index]))
+        index+=1
+    return list
 
 class Logic(BoxLayout):
     def __init__(self, **kwargs):
@@ -61,19 +86,19 @@ class Grapher(Graph):
     def __init__(self, **kwargs):
         super(Grapher, self).__init__(**kwargs)
         self.plot = LinePlot(line_width=2.0, color=[0, 0, 1, 1])
-        self.plot2 = LinePlot(line_width=1.5, color=[1, 0, 0, 0.6])
+        self.plot2 = LinePlot(line_width=1.5, color=[1, 0, 0, 0.5])
         self.add_plot(self.plot)
         self.add_plot(self.plot2)
         self.ymax=1023
-        self.xmax=x_amount
+        self.xmax=graphTime
         Clock.schedule_interval(self.get_value, 0.001)
         get_level_thread = Thread(target = get_data)
         get_level_thread.daemon = True
         get_level_thread.start()
     
     def get_value(self, dt):
-        self.plot.points = [(i, j) for i, j in enumerate(levels)]
-        self.plot2.points = [(i, j) for i, j in enumerate(pastLevels)]
+        self.plot.points = combineLists(times,levels)
+        self.plot2.points = combineLists(oldTime,oldLevels)
 
 class BreathEasy(App):
     def build(self):
