f5eafb098373a0c0de3b248c2af7d794a5851ab8
Alon Nusem <61749872+4lon@users.noreply.github.com>
2020-04-16 10:31:44 +1000

    serialTest

--- Test Files/serialTest.py
+++ Test Files/serialTest.py
@@ -4,15 +4,23 @@ import matplotlib.pyplot as plt
 import serial
  
 ser = serial.Serial('COM3', 9600)
+mode = 2
+if mode==1:
+    while True:
+        while (ser.inWaiting()==0):
+            pass
+        value = ser.readline()
+        try:
+            data = str(value.decode("utf-8"))
+            data=data.split(",")
+            signal1 = int(data[1])
+            print(signal1)
+        except:
+            pass
 
-while True:
-    while (ser.inWaiting()==0):
-        pass
-    value = ser.readline()
-    try:
-        data = str(value.decode("utf-8"))
-        data=data.split(",")
-        signal1 = int(data[1])
-        print(signal1)
-    except:
-        pass
+elif mode==2:
+    ser.write("Hello from Python!".encode())
+    while True:
+        data = ser.readline()
+        if data:
+            print(data.rstrip('\n'))
